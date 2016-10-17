cbff892ef1d2d1f937c17a2841db91edece0faf9
Jakub Kułak <jakub.kulak@gmail.com>
2016-10-16 21:33:48 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jakub.kulak@gmail.com'
 license          'All rights reserved'
 description      'Wrappers for most commonly used community cookbooks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'apache2', '~> 3.2.2'
 depends 'php', '~> 1.10.1'
